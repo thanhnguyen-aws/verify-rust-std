rust-lang/libm

Replace `HasDomain` to enable multi-argument edge case and domain tests
This also allows reusing the same generator logic between logspace tests
and extensive tests, so comes with a nice bit of cleanup.

Changes:

* Make the generator part of `CheckCtx` since a `Generator` and
  `CheckCtx` are almost always passed together.
* Rename `domain_logspace` to `spaced` since this no longer only
  operates within a domain and we may want to handle integer spacing.
* Domain is now calculated at runtime rather than using traits, which is
  much easier to work with.
* With the above, domains for multidimensional functions are added.
* The extensive test generator code tests has been combined with the
  domain_logspace generator code. With this, the domain tests have just
  become a subset of extensive tests. These were renamed to "quickspace"
  since, technically, the extensive tests are also "domain" or "domain
  logspace" tests.
* Edge case generators now handle functions with multiple inputs.
* The test runners can be significantly cleaned up and deduplicated.

crates/libm-test/src/domain.rs
@@ -1,11 +1,13 @@
 //! Traits and operations related to bounds of a function.
 
 use std::fmt;
-use std::ops::{self, Bound};
+use std::ops::Bound;
 
-use crate::{Float, FloatExt};
+use libm::support::Int;
 
-/// Representation of a function's domain.
+use crate::{BaseName, Float, FloatExt, Identifier};
+
+/// Representation of a single dimension of a function's domain.
 #[derive(Clone, Debug)]
 pub struct Domain<T> {
     /// Start of the region for which a function is defined (ignoring poles).
@@ -39,56 +41,129 @@ impl<F: FloatExt> Domain<F> {
     }
 }
 
+/// A value that may be any float type or any integer type.
+#[derive(Clone, Debug)]
+pub enum EitherPrim<F, I> {
+    Float(F),
+    Int(I),
+}
+
+impl<F: fmt::Debug, I: fmt::Debug> EitherPrim<F, I> {
+    pub fn unwrap_float(self) -> F {
+        match self {
+            EitherPrim::Float(f) => f,
+            EitherPrim::Int(_) => panic!("expected float; got {self:?}"),
+        }
+    }
+
+    pub fn unwrap_int(self) -> I {
+        match self {
+            EitherPrim::Float(_) => panic!("expected int; got {self:?}"),
+            EitherPrim::Int(i) => i,
+        }
+    }
+}
+
+/// Convenience 1-dimensional float domains.
 impl<F: Float> Domain<F> {
     /// x ∈ ℝ
-    pub const UNBOUNDED: Self =
+    const UNBOUNDED: Self =
         Self { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None };
 
     /// x ∈ ℝ >= 0
-    pub const POSITIVE: Self =
+    const POSITIVE: Self =
         Self { start: Bound::Included(F::ZERO), end: Bound::Unbounded, check_points: None };
 
     /// x ∈ ℝ > 0
-    pub const STRICTLY_POSITIVE: Self =
+    const STRICTLY_POSITIVE: Self =
         Self { start: Bound::Excluded(F::ZERO), end: Bound::Unbounded, check_points: None };
 
+    const fn into_prim_f<I>(self) -> EitherPrim<Self, Domain<I>> {
+        EitherPrim::Float(self)
+    }
+}
+
+/// Convenience 1-dimensional integer domains.
+impl<I: Int> Domain<I> {
+    /// x ∈ ℝ
+    const UNBOUNDED_INT: Self =
+        Self { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None };
+
+    const fn into_prim_i<F>(self) -> EitherPrim<Domain<F>, Self> {
+        EitherPrim::Int(self)
+    }
+}
+
+/// Multidimensional domains, represented as an array of 1-D domains.
+impl<F: Float, I: Int> EitherPrim<Domain<F>, Domain<I>> {
+    /// x ∈ ℝ
+    const UNBOUNDED1: [Self; 1] =
+        [Domain { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None }
+            .into_prim_f()];
+
+    /// {x1, x2} ∈ ℝ
+    const UNBOUNDED2: [Self; 2] =
+        [Domain::UNBOUNDED.into_prim_f(), Domain::UNBOUNDED.into_prim_f()];
+
+    /// {x1, x2, x3} ∈ ℝ
+    const UNBOUNDED3: [Self; 3] = [
+        Domain::UNBOUNDED.into_prim_f(),
+        Domain::UNBOUNDED.into_prim_f(),
+        Domain::UNBOUNDED.into_prim_f(),
+    ];
+
+    /// {x1, x2} ∈ ℝ, one float and one int
+    const UNBOUNDED_F_I: [Self; 2] =
+        [Domain::UNBOUNDED.into_prim_f(), Domain::UNBOUNDED_INT.into_prim_i()];
+
+    /// x ∈ ℝ >= 0
+    const POSITIVE: [Self; 1] = [Domain::POSITIVE.into_prim_f()];
+
+    /// x ∈ ℝ > 0
+    const STRICTLY_POSITIVE: [Self; 1] = [Domain::STRICTLY_POSITIVE.into_prim_f()];
+
     /// Used for versions of `asin` and `acos`.
-    pub const INVERSE_TRIG_PERIODIC: Self = Self {
+    const INVERSE_TRIG_PERIODIC: [Self; 1] = [Domain {
         start: Bound::Included(F::NEG_ONE),
         end: Bound::Included(F::ONE),
         check_points: None,
-    };
+    }
+    .into_prim_f()];
 
     /// Domain for `acosh`
-    pub const ACOSH: Self =
-        Self { start: Bound::Included(F::ONE), end: Bound::Unbounded, check_points: None };
+    const ACOSH: [Self; 1] =
+        [Domain { start: Bound::Included(F::ONE), end: Bound::Unbounded, check_points: None }
+            .into_prim_f()];
 
     /// Domain for `atanh`
-    pub const ATANH: Self = Self {
+    const ATANH: [Self; 1] = [Domain {
         start: Bound::Excluded(F::NEG_ONE),
         end: Bound::Excluded(F::ONE),
         check_points: None,
-    };
+    }
+    .into_prim_f()];
 
     /// Domain for `sin`, `cos`, and `tan`
-    pub const TRIG: Self = Self {
+    const TRIG: [Self; 1] = [Domain {
         // TODO
         check_points: Some(|| Box::new([-F::PI, -F::FRAC_PI_2, F::FRAC_PI_2, F::PI].into_iter())),
-        ..Self::UNBOUNDED
-    };
+        ..Domain::UNBOUNDED
+    }
+    .into_prim_f()];
 
     /// Domain for `log` in various bases
-    pub const LOG: Self = Self::STRICTLY_POSITIVE;
+    const LOG: [Self; 1] = Self::STRICTLY_POSITIVE;
 
     /// Domain for `log1p` i.e. `log(1 + x)`
-    pub const LOG1P: Self =
-        Self { start: Bound::Excluded(F::NEG_ONE), end: Bound::Unbounded, check_points: None };
+    const LOG1P: [Self; 1] =
+        [Domain { start: Bound::Excluded(F::NEG_ONE), end: Bound::Unbounded, check_points: None }
+            .into_prim_f()];
 
     /// Domain for `sqrt`
-    pub const SQRT: Self = Self::POSITIVE;
+    const SQRT: [Self; 1] = Self::POSITIVE;
 
     /// Domain for `gamma`
-    pub const GAMMA: Self = Self {
+    const GAMMA: [Self; 1] = [Domain {
         check_points: Some(|| {
             // Negative integers are asymptotes
             Box::new((0..u8::MAX).map(|scale| {
@@ -100,122 +175,84 @@ impl<F: Float> Domain<F> {
             }))
         }),
         // Whether or not gamma is defined for negative numbers is implementation dependent
-        ..Self::UNBOUNDED
-    };
+        ..Domain::UNBOUNDED
+    }
+    .into_prim_f()];
 
     /// Domain for `loggamma`
-    pub const LGAMMA: Self = Self::STRICTLY_POSITIVE;
+    const LGAMMA: [Self; 1] = Self::STRICTLY_POSITIVE;
+
+    /// Domain for `jn` and `yn`.
+    // FIXME: the domain should provide some sort of "reasonable range" so we don't actually test
+    // the entire system unbounded.
+    const BESSEL_N: [Self; 2] =
+        [Domain::UNBOUNDED_INT.into_prim_i(), Domain::UNBOUNDED.into_prim_f()];
 }
 
-/// Implement on `op::*` types to indicate how they are bounded.
-pub trait HasDomain<T>
-where
-    T: Copy + fmt::Debug + ops::Add<Output = T> + ops::Sub<Output = T> + PartialOrd + 'static,
-{
-    const DOMAIN: Domain<T>;
-}
-
-/// Implement [`HasDomain`] for both the `f32` and `f64` variants of a function.
-macro_rules! impl_has_domain {
-    ($($fn_name:ident => $domain:expr;)*) => {
-        paste::paste! {
-            $(
-                // Implement for f64 functions
-                impl HasDomain<f64> for $crate::op::$fn_name::Routine {
-                    const DOMAIN: Domain<f64> = Domain::<f64>::$domain;
-                }
-
-                // Implement for f32 functions
-                impl HasDomain<f32> for $crate::op::[< $fn_name f >]::Routine {
-                    const DOMAIN: Domain<f32> = Domain::<f32>::$domain;
-                }
-            )*
-        }
+/// Get the domain for a given function.
+pub fn get_domain<F: Float, I: Int>(
+    id: Identifier,
+    argnum: usize,
+) -> EitherPrim<Domain<F>, Domain<I>> {
+    let x = match id.base_name() {
+        BaseName::Acos => &EitherPrim::INVERSE_TRIG_PERIODIC[..],
+        BaseName::Acosh => &EitherPrim::ACOSH[..],
+        BaseName::Asin => &EitherPrim::INVERSE_TRIG_PERIODIC[..],
+        BaseName::Asinh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atan => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atan2 => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Cbrt => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atanh => &EitherPrim::ATANH[..],
+        BaseName::Ceil => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Cosh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Copysign => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Cos => &EitherPrim::TRIG[..],
+        BaseName::Exp => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Erf => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Erfc => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Expm1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Exp10 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Exp2 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Frexp => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fabs => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fdim => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Floor => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fma => &EitherPrim::UNBOUNDED3[..],
+        BaseName::Fmax => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Fmin => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Fmod => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Hypot => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Ilogb => &EitherPrim::UNBOUNDED1[..],
+        BaseName::J0 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::J1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Jn => &EitherPrim::BESSEL_N[..],
+        BaseName::Ldexp => &EitherPrim::UNBOUNDED_F_I[..],
+        BaseName::Lgamma => &EitherPrim::LGAMMA[..],
+        BaseName::LgammaR => &EitherPrim::LGAMMA[..],
+        BaseName::Log => &EitherPrim::LOG[..],
+        BaseName::Log10 => &EitherPrim::LOG[..],
+        BaseName::Log1p => &EitherPrim::LOG1P[..],
+        BaseName::Log2 => &EitherPrim::LOG[..],
+        BaseName::Modf => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Nextafter => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Pow => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Remainder => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Remquo => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Rint => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Round => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Scalbn => &EitherPrim::UNBOUNDED_F_I[..],
+        BaseName::Sin => &EitherPrim::TRIG[..],
+        BaseName::Sincos => &EitherPrim::TRIG[..],
+        BaseName::Sinh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Sqrt => &EitherPrim::SQRT[..],
+        BaseName::Tan => &EitherPrim::TRIG[..],
+        BaseName::Tanh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Tgamma => &EitherPrim::GAMMA[..],
+        BaseName::Trunc => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Y0 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Y1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Yn => &EitherPrim::BESSEL_N[..],
     };
-}
 
-// Tie functions together with their domains.
-impl_has_domain! {
-    acos => INVERSE_TRIG_PERIODIC;
-    acosh => ACOSH;
-    asin => INVERSE_TRIG_PERIODIC;
-    asinh => UNBOUNDED;
-    atan => UNBOUNDED;
-    atanh => ATANH;
-    cbrt => UNBOUNDED;
-    ceil => UNBOUNDED;
-    cos => TRIG;
-    cosh => UNBOUNDED;
-    erf => UNBOUNDED;
-    erfc => UNBOUNDED;
-    exp => UNBOUNDED;
-    exp10 => UNBOUNDED;
-    exp2 => UNBOUNDED;
-    expm1 => UNBOUNDED;
-    fabs => UNBOUNDED;
-    floor => UNBOUNDED;
-    frexp => UNBOUNDED;
-    ilogb => UNBOUNDED;
-    j0 => UNBOUNDED;
-    j1 => UNBOUNDED;
-    lgamma => LGAMMA;
-    log => LOG;
-    log10 => LOG;
-    log1p => LOG1P;
-    log2 => LOG;
-    modf => UNBOUNDED;
-    rint => UNBOUNDED;
-    round => UNBOUNDED;
-    sin => TRIG;
-    sincos => TRIG;
-    sinh => UNBOUNDED;
-    sqrt => SQRT;
-    tan => TRIG;
-    tanh => UNBOUNDED;
-    tgamma => GAMMA;
-    trunc => UNBOUNDED;
-    y0 => UNBOUNDED;
-    y1 => UNBOUNDED;
-}
-
-/* Manual implementations, these functions don't follow `foo`->`foof` naming */
-
-impl HasDomain<f32> for crate::op::lgammaf_r::Routine {
-    const DOMAIN: Domain<f32> = Domain::<f32>::LGAMMA;
-}
-
-impl HasDomain<f64> for crate::op::lgamma_r::Routine {
-    const DOMAIN: Domain<f64> = Domain::<f64>::LGAMMA;
-}
-
-/* Not all `f16` and `f128` functions exist yet so we can't easily use the macros. */
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::fabsf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::fabsf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
-}
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::fdimf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::fdimf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
-}
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::truncf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::truncf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
+    x[argnum].clone()
 }

thanhnguyen-aws/verify-rust-std

compiler-builtins/libm/crates/libm-test/src/domain.rs
@@ -1,11 +1,13 @@
 //! Traits and operations related to bounds of a function.
 
 use std::fmt;
-use std::ops::{self, Bound};
+use std::ops::Bound;
 
-use crate::{Float, FloatExt};
+use libm::support::Int;
 
-/// Representation of a function's domain.
+use crate::{BaseName, Float, FloatExt, Identifier};
+
+/// Representation of a single dimension of a function's domain.
 #[derive(Clone, Debug)]
 pub struct Domain<T> {
     /// Start of the region for which a function is defined (ignoring poles).
@@ -39,56 +41,131 @@ impl<F: FloatExt> Domain<F> {
     }
 }
 
+/// A value that may be any float type or any integer type.
+#[derive(Clone, Debug)]
+pub enum EitherPrim<F, I> {
+    Float(F),
+    Int(I),
+}
+
+impl<F: fmt::Debug, I: fmt::Debug> EitherPrim<F, I> {
+    pub fn unwrap_float(self) -> F {
+        match self {
+            EitherPrim::Float(f) => f,
+            EitherPrim::Int(_) => panic!("expected float; got {self:?}"),
+        }
+    }
+
+    pub fn unwrap_int(self) -> I {
+        match self {
+            EitherPrim::Float(_) => panic!("expected int; got {self:?}"),
+            EitherPrim::Int(i) => i,
+        }
+    }
+}
+
+/// Convenience 1-dimensional float domains.
 impl<F: Float> Domain<F> {
     /// x ∈ ℝ
-    pub const UNBOUNDED: Self =
+    const UNBOUNDED: Self =
         Self { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None };
 
     /// x ∈ ℝ >= 0
-    pub const POSITIVE: Self =
+    const POSITIVE: Self =
         Self { start: Bound::Included(F::ZERO), end: Bound::Unbounded, check_points: None };
 
     /// x ∈ ℝ > 0
-    pub const STRICTLY_POSITIVE: Self =
+    const STRICTLY_POSITIVE: Self =
         Self { start: Bound::Excluded(F::ZERO), end: Bound::Unbounded, check_points: None };
 
+    /// Wrap in the float variant of [`EitherPrim`].
+    const fn into_prim_float<I>(self) -> EitherPrim<Self, Domain<I>> {
+        EitherPrim::Float(self)
+    }
+}
+
+/// Convenience 1-dimensional integer domains.
+impl<I: Int> Domain<I> {
+    /// x ∈ ℝ
+    const UNBOUNDED_INT: Self =
+        Self { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None };
+
+    /// Wrap in the int variant of [`EitherPrim`].
+    const fn into_prim_int<F>(self) -> EitherPrim<Domain<F>, Self> {
+        EitherPrim::Int(self)
+    }
+}
+
+/// Multidimensional domains, represented as an array of 1-D domains.
+impl<F: Float, I: Int> EitherPrim<Domain<F>, Domain<I>> {
+    /// x ∈ ℝ
+    const UNBOUNDED1: [Self; 1] =
+        [Domain { start: Bound::Unbounded, end: Bound::Unbounded, check_points: None }
+            .into_prim_float()];
+
+    /// {x1, x2} ∈ ℝ
+    const UNBOUNDED2: [Self; 2] =
+        [Domain::UNBOUNDED.into_prim_float(), Domain::UNBOUNDED.into_prim_float()];
+
+    /// {x1, x2, x3} ∈ ℝ
+    const UNBOUNDED3: [Self; 3] = [
+        Domain::UNBOUNDED.into_prim_float(),
+        Domain::UNBOUNDED.into_prim_float(),
+        Domain::UNBOUNDED.into_prim_float(),
+    ];
+
+    /// {x1, x2} ∈ ℝ, one float and one int
+    const UNBOUNDED_F_I: [Self; 2] =
+        [Domain::UNBOUNDED.into_prim_float(), Domain::UNBOUNDED_INT.into_prim_int()];
+
+    /// x ∈ ℝ >= 0
+    const POSITIVE: [Self; 1] = [Domain::POSITIVE.into_prim_float()];
+
+    /// x ∈ ℝ > 0
+    const STRICTLY_POSITIVE: [Self; 1] = [Domain::STRICTLY_POSITIVE.into_prim_float()];
+
     /// Used for versions of `asin` and `acos`.
-    pub const INVERSE_TRIG_PERIODIC: Self = Self {
+    const INVERSE_TRIG_PERIODIC: [Self; 1] = [Domain {
         start: Bound::Included(F::NEG_ONE),
         end: Bound::Included(F::ONE),
         check_points: None,
-    };
+    }
+    .into_prim_float()];
 
     /// Domain for `acosh`
-    pub const ACOSH: Self =
-        Self { start: Bound::Included(F::ONE), end: Bound::Unbounded, check_points: None };
+    const ACOSH: [Self; 1] =
+        [Domain { start: Bound::Included(F::ONE), end: Bound::Unbounded, check_points: None }
+            .into_prim_float()];
 
     /// Domain for `atanh`
-    pub const ATANH: Self = Self {
+    const ATANH: [Self; 1] = [Domain {
         start: Bound::Excluded(F::NEG_ONE),
         end: Bound::Excluded(F::ONE),
         check_points: None,
-    };
+    }
+    .into_prim_float()];
 
     /// Domain for `sin`, `cos`, and `tan`
-    pub const TRIG: Self = Self {
-        // TODO
+    const TRIG: [Self; 1] = [Domain {
+        // Trig functions have special behavior at fractions of π.
         check_points: Some(|| Box::new([-F::PI, -F::FRAC_PI_2, F::FRAC_PI_2, F::PI].into_iter())),
-        ..Self::UNBOUNDED
-    };
+        ..Domain::UNBOUNDED
+    }
+    .into_prim_float()];
 
     /// Domain for `log` in various bases
-    pub const LOG: Self = Self::STRICTLY_POSITIVE;
+    const LOG: [Self; 1] = Self::STRICTLY_POSITIVE;
 
     /// Domain for `log1p` i.e. `log(1 + x)`
-    pub const LOG1P: Self =
-        Self { start: Bound::Excluded(F::NEG_ONE), end: Bound::Unbounded, check_points: None };
+    const LOG1P: [Self; 1] =
+        [Domain { start: Bound::Excluded(F::NEG_ONE), end: Bound::Unbounded, check_points: None }
+            .into_prim_float()];
 
     /// Domain for `sqrt`
-    pub const SQRT: Self = Self::POSITIVE;
+    const SQRT: [Self; 1] = Self::POSITIVE;
 
     /// Domain for `gamma`
-    pub const GAMMA: Self = Self {
+    const GAMMA: [Self; 1] = [Domain {
         check_points: Some(|| {
             // Negative integers are asymptotes
             Box::new((0..u8::MAX).map(|scale| {
@@ -100,122 +177,84 @@ impl<F: Float> Domain<F> {
             }))
         }),
         // Whether or not gamma is defined for negative numbers is implementation dependent
-        ..Self::UNBOUNDED
-    };
+        ..Domain::UNBOUNDED
+    }
+    .into_prim_float()];
 
     /// Domain for `loggamma`
-    pub const LGAMMA: Self = Self::STRICTLY_POSITIVE;
+    const LGAMMA: [Self; 1] = Self::STRICTLY_POSITIVE;
+
+    /// Domain for `jn` and `yn`.
+    // FIXME: the domain should provide some sort of "reasonable range" so we don't actually test
+    // the entire system unbounded.
+    const BESSEL_N: [Self; 2] =
+        [Domain::UNBOUNDED_INT.into_prim_int(), Domain::UNBOUNDED.into_prim_float()];
 }
 
-/// Implement on `op::*` types to indicate how they are bounded.
-pub trait HasDomain<T>
-where
-    T: Copy + fmt::Debug + ops::Add<Output = T> + ops::Sub<Output = T> + PartialOrd + 'static,
-{
-    const DOMAIN: Domain<T>;
-}
-
-/// Implement [`HasDomain`] for both the `f32` and `f64` variants of a function.
-macro_rules! impl_has_domain {
-    ($($fn_name:ident => $domain:expr;)*) => {
-        paste::paste! {
-            $(
-                // Implement for f64 functions
-                impl HasDomain<f64> for $crate::op::$fn_name::Routine {
-                    const DOMAIN: Domain<f64> = Domain::<f64>::$domain;
-                }
-
-                // Implement for f32 functions
-                impl HasDomain<f32> for $crate::op::[< $fn_name f >]::Routine {
-                    const DOMAIN: Domain<f32> = Domain::<f32>::$domain;
-                }
-            )*
-        }
+/// Get the domain for a given function.
+pub fn get_domain<F: Float, I: Int>(
+    id: Identifier,
+    argnum: usize,
+) -> EitherPrim<Domain<F>, Domain<I>> {
+    let x = match id.base_name() {
+        BaseName::Acos => &EitherPrim::INVERSE_TRIG_PERIODIC[..],
+        BaseName::Acosh => &EitherPrim::ACOSH[..],
+        BaseName::Asin => &EitherPrim::INVERSE_TRIG_PERIODIC[..],
+        BaseName::Asinh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atan => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atan2 => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Cbrt => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Atanh => &EitherPrim::ATANH[..],
+        BaseName::Ceil => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Cosh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Copysign => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Cos => &EitherPrim::TRIG[..],
+        BaseName::Exp => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Erf => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Erfc => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Expm1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Exp10 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Exp2 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Frexp => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fabs => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fdim => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Floor => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Fma => &EitherPrim::UNBOUNDED3[..],
+        BaseName::Fmax => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Fmin => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Fmod => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Hypot => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Ilogb => &EitherPrim::UNBOUNDED1[..],
+        BaseName::J0 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::J1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Jn => &EitherPrim::BESSEL_N[..],
+        BaseName::Ldexp => &EitherPrim::UNBOUNDED_F_I[..],
+        BaseName::Lgamma => &EitherPrim::LGAMMA[..],
+        BaseName::LgammaR => &EitherPrim::LGAMMA[..],
+        BaseName::Log => &EitherPrim::LOG[..],
+        BaseName::Log10 => &EitherPrim::LOG[..],
+        BaseName::Log1p => &EitherPrim::LOG1P[..],
+        BaseName::Log2 => &EitherPrim::LOG[..],
+        BaseName::Modf => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Nextafter => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Pow => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Remainder => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Remquo => &EitherPrim::UNBOUNDED2[..],
+        BaseName::Rint => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Round => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Scalbn => &EitherPrim::UNBOUNDED_F_I[..],
+        BaseName::Sin => &EitherPrim::TRIG[..],
+        BaseName::Sincos => &EitherPrim::TRIG[..],
+        BaseName::Sinh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Sqrt => &EitherPrim::SQRT[..],
+        BaseName::Tan => &EitherPrim::TRIG[..],
+        BaseName::Tanh => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Tgamma => &EitherPrim::GAMMA[..],
+        BaseName::Trunc => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Y0 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Y1 => &EitherPrim::UNBOUNDED1[..],
+        BaseName::Yn => &EitherPrim::BESSEL_N[..],
     };
-}
 
-// Tie functions together with their domains.
-impl_has_domain! {
-    acos => INVERSE_TRIG_PERIODIC;
-    acosh => ACOSH;
-    asin => INVERSE_TRIG_PERIODIC;
-    asinh => UNBOUNDED;
-    atan => UNBOUNDED;
-    atanh => ATANH;
-    cbrt => UNBOUNDED;
-    ceil => UNBOUNDED;
-    cos => TRIG;
-    cosh => UNBOUNDED;
-    erf => UNBOUNDED;
-    erfc => UNBOUNDED;
-    exp => UNBOUNDED;
-    exp10 => UNBOUNDED;
-    exp2 => UNBOUNDED;
-    expm1 => UNBOUNDED;
-    fabs => UNBOUNDED;
-    floor => UNBOUNDED;
-    frexp => UNBOUNDED;
-    ilogb => UNBOUNDED;
-    j0 => UNBOUNDED;
-    j1 => UNBOUNDED;
-    lgamma => LGAMMA;
-    log => LOG;
-    log10 => LOG;
-    log1p => LOG1P;
-    log2 => LOG;
-    modf => UNBOUNDED;
-    rint => UNBOUNDED;
-    round => UNBOUNDED;
-    sin => TRIG;
-    sincos => TRIG;
-    sinh => UNBOUNDED;
-    sqrt => SQRT;
-    tan => TRIG;
-    tanh => UNBOUNDED;
-    tgamma => GAMMA;
-    trunc => UNBOUNDED;
-    y0 => UNBOUNDED;
-    y1 => UNBOUNDED;
-}
-
-/* Manual implementations, these functions don't follow `foo`->`foof` naming */
-
-impl HasDomain<f32> for crate::op::lgammaf_r::Routine {
-    const DOMAIN: Domain<f32> = Domain::<f32>::LGAMMA;
-}
-
-impl HasDomain<f64> for crate::op::lgamma_r::Routine {
-    const DOMAIN: Domain<f64> = Domain::<f64>::LGAMMA;
-}
-
-/* Not all `f16` and `f128` functions exist yet so we can't easily use the macros. */
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::fabsf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::fabsf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
-}
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::fdimf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::fdimf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
-}
-
-#[cfg(f16_enabled)]
-impl HasDomain<f16> for crate::op::truncf16::Routine {
-    const DOMAIN: Domain<f16> = Domain::<f16>::UNBOUNDED;
-}
-
-#[cfg(f128_enabled)]
-impl HasDomain<f128> for crate::op::truncf128::Routine {
-    const DOMAIN: Domain<f128> = Domain::<f128>::UNBOUNDED;
+    x[argnum].clone()
 }